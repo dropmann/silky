1f3709d3a40f856b614fabf229697faa8e0c6fd4
Damian Dropmann <damiandropmann@gmail.com>
2022-10-21 15:34:31 +1100

    improvements for ipad usage
    - fixed issue with entering data into spreadsheet
    - fixed issue with context menus not appearing

--- client/resultsview/main.js
+++ client/resultsview/main.js
@@ -14,6 +14,7 @@ const Annotations = require('./annotations');
 const Tracker = require('./itemtracker');
 const I18n = require("../common/i18n");
 const focusLoop = require('../common/focusloop');
+const { contextMenuListener } = require('../common/utils');
 
 window._ = I18n._;
 
@@ -88,7 +89,7 @@ class Main {  // this is constructed at the bottom
         $(document).mouseup(this, (event) => this._mouseUp(event));
         $(document).mousemove(this, (event) => this._mouseMove(event));
 
-        document.body.addEventListener('contextmenu', (event) => {
+        contextMenuListener(document.body, (event) => {
             let clickEvent = $.Event('contextmenu');
             clickEvent.pageX = event.pageX;
             clickEvent.pageY = event.pageY;
@@ -211,10 +212,14 @@ class Main {  // this is constructed at the bottom
             let el = document.elementFromPoint(hostEvent.pageX, hostEvent.pageY);
             if (el === document.body)
                 el = this.$results[0];
-            let clickEvent = $.Event('contextmenu');
-            clickEvent.pageX = hostEvent.pageX;
-            clickEvent.pageY = hostEvent.pageY;
-            $(el).trigger(clickEvent);
+            const clickEvent = new MouseEvent('contextmenu', {
+                view: window,
+                bubbles: true,
+                cancelable: true,
+                clientX: hostEvent.pageX,
+                clientY: hostEvent.pageY,
+            });
+            el.dispatchEvent(clickEvent);
         }
         else if (hostEvent.type === 'enterannotation') {
             let annotation = Annotations.controls[0];
