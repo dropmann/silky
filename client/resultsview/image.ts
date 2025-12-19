c6a877ea2a27a2aa58ce9fc6d92113102c079e90
Damian Dropmann <damiandropmann@gmail.com>
2025-12-19 15:06:20 +1100

    Added image resizing UI

--- client/resultsview/image.ts
+++ client/resultsview/image.ts
@@ -11,7 +11,11 @@ import { AnalysisStatus } from './create';
 export interface IImageElementData {
     path: string,
     width: number,
-    height: number
+    height: number,
+    widthM: number,
+    widthB: number,
+    heightM: number,
+    heightB: number
 }
 
 export class Model extends Elem.Model<ElementModel<IImageElementData>> {
@@ -23,7 +27,11 @@ export class Model extends Elem.Model<ElementModel<IImageElementData>> {
             element: {
                 path: '',
                 width: 400,
-                height: 300
+                height: 300,
+                widthM: 1,
+                widthB: 0,
+                heightM: 1,
+                heightB: 0
             },
             error: null,
             status: AnalysisStatus.ANALYSIS_COMPLETE,
@@ -37,6 +45,15 @@ export class View extends Elem.View<Model> {
 
     $title: HTMLHeadingElement;
     $image: HTMLElement;
+    $size: HTMLElement;
+    resizeObserver: ResizeObserver;
+
+    updating: boolean = false;
+
+    widthOfImage = -1;
+    heightOfImage = -1;
+    initalised = false;
+
 
     constructor(model: Model, data: ElementData) {
         super(model, data);
@@ -60,7 +77,77 @@ export class View extends Elem.View<Model> {
         this.$image = HTML.parse(`<div class="jmv-results-image-image" data-address="${ encodeURI(address) }">`);
         this.append(this.$image);
 
+        this.$size = HTML.parse('<div class="size-display"></div>');
+        this.append(this.$size);
+
+        this.resizeObserver = new ResizeObserver((entries) => {
+            for (const entry of entries) {
+                if (entry.contentBoxSize) {
+                    const contentBoxSize = entry.contentBoxSize[0];
+                    this.$image.style.backgroundSize = '';
+
+                    const different = this.widthOfImage !== entry.contentRect.width || this.heightOfImage !== entry.contentRect.height;
+                    this.widthOfImage = entry.contentRect.width;
+                    this.heightOfImage = entry.contentRect.height;
+                    
+                    if (this.initalised && different)
+                        this.updating = true;
+
+                    this.initalised = true;
+
+                    this.updateSizeDisplay();
+                }
+            }
+        });
+
+        this.applyScaleValues = this.applyScaleValues.bind(this);
+        this.imagePointerDown = this.imagePointerDown.bind(this);
+    }
+
+    disconnectedCallback() {
+        this.$image.removeEventListener('pointerdown',  this.imagePointerDown);
+        document.removeEventListener('pointerup', this.applyScaleValues);
+        this.resizeObserver.unobserve(this.$image);
+    }
+
+    connectedCallback() {
+        this.$image.addEventListener('pointerdown',  this.imagePointerDown);
+        document.addEventListener('pointerup', this.applyScaleValues);
         this.render();
+    }
+
+    imagePointerDown() {
+        if (this.initalised === false)
+            this.resizeObserver.observe(this.$image);
+
+        this.$size.style.opacity = '1';
+        this.updateSizeDisplay();
+    }
+
+    updateSizeDisplay() {
+        this.$size.innerText = `${this.widthOfImage} x ${this.heightOfImage}`;
+    }
+
+    applyScaleValues() {
+        this.$size.style.opacity = '0';
+        if (this.updating) {
+            this.updateScaleValues(this.widthOfImage, this.heightOfImage);
+            this.updating = false;
+        }
+    }
+
+    updateScaleValues(widthOfImage: number, heightOfImage: number) {
+        let element = this.model.attributes.element
+
+        let widthM = element.widthM === 0 ? widthOfImage : element.widthM;
+        let heightM = element.heightM === 0 ? heightOfImage : element.heightM;
+
+        const widthScale = (widthOfImage - element.widthB) / widthM;
+        const heightScale = (heightOfImage - element.heightB) / heightM;
+
+        window.setParam(this.address(), { widthScale, heightScale });
+
+        //console.log(`${widthScale}, ${heightScale}`);
     }
 
     type() {
@@ -72,7 +159,6 @@ export class View extends Elem.View<Model> {
     }
 
     render() {
-
         if (this.$title) {
             if (this.model.attributes.title) {
                 this.$title.textContent = this.model.attributes.title;
@@ -108,7 +194,6 @@ export class View extends Elem.View<Model> {
         this.$image.style.width = element.width + 'px';
         this.$image.style.height = element.height + 'px';
         this.$image.style.backgroundSize = element.width + 'px';
-
     }
 }
 
